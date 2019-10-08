c60e054704aaf79adbbfd4c8da605e101cdb8494
carrotWu <542425997@qq.com>
2019-10-08 22:35:21 +0800

    chore: 修改tsconfig配置

--- alias.config.js
+++ alias.config.js
@@ -3,15 +3,14 @@ const path = require('path')
 module.exports = {
   resolve: {
     alias: {
-      SRC: path.resolve(__dirname, "src"),
-      ASSET: path.resolve(__dirname, "assets"),
-      COMPONENT: path.resolve(__dirname, "src/components"),
-      CONST: path.resolve(__dirname, "src/constants"),
-      MODEL: path.resolve(__dirname, "src/models"),
-      PAGE: path.resolve(__dirname, "src/pages"),
-      ROUTE: path.resolve(__dirname, "src/routes"),
-      SERVICE: path.resolve(__dirname, "src/services"),
-      UTIL: path.resolve(__dirname, "src/utils"),
+      '@': path.resolve(__dirname, "src"),
+      'ASSET': path.resolve(__dirname, "assets"),
+      'COMPONENT': path.resolve(__dirname, "src/components"),
+      'CONST': path.resolve(__dirname, "src/constants"),
+      'ROUTE': path.resolve(__dirname, "src/routes"),
+      'SERVICE': path.resolve(__dirname, "src/services"),
+      'UTIL': path.resolve(__dirname, "src/utils"),
+      'VIEW': path.resolve(__dirname, "src/view"),
     }
   }
 }
