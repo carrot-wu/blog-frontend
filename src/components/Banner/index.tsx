3a83eea5e831c8aa93371162642f056cd0d2d9da
吴泓锐 <wuyuanrui@xiao100.com>
2019-11-15 15:00:56 +0800

    chore: 修改部分样式以及图片链接

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,7 +3,7 @@ import './styles.less'
 export default function Banner() {
   return (
     <div className="banner">
-      <img src="http://pzxbfjvwp.bkt.clouddn.com/FvKr_PFFe8uyWhqwbZMHjKw3tARK" alt=""/>
+      <img src="http://img.carrotwu.com/FvKr_PFFe8uyWhqwbZMHjKw3tARK" alt=""/>
     </div>
   )
 }
