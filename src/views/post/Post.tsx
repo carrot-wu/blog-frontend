224d5715763b85f9112cc70dc9c66e1282f741a2
wuhongrui <wuhongrui@ruqimobility.com>
2020-08-04 17:10:23 +0800

    fix

--- src/views/post/Post.tsx
+++ src/views/post/Post.tsx
@@ -1,152 +1,24 @@
-import marked from "marked";
-import React from "react";
-
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-})
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-})
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-})
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-})
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-})
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-})
-marked.setOptions({
-  highlight(code: string, lang: any) {
-    return Prism.highlight(code, Prism.languages[lang], lang)
-  }
-  marked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = () ny) {
-  return Prism.highlight(code, Prism.languages[lang], lang)
-}
-})
-const Post:React.FC = () => {
-  const {id} = useParams()
-  const history = useHistory()
-  const { promiseFn: getArticleDetail, res: {data}, loading } = usePromise(
-    <div className="title">{tit:React.FC = () ny) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-    })
-      const Post:React.FC = () => {
-        const {id} = useParams()
-        const history = useHistory()
-        const { promiseFn: getArticleDetail, res: {data}, loading } = usePromise(
-        <div className="title">{title}</div>
-  <div className="byline">
-    <div className="author">carrotWu</div>
-    <div className="time">{time}</div>
-  </div>
-  <div className="markdown-body" dangerouslySetInnerHTML={{__html:html}}/>
-</div>
-}
-    }
-    // eslint-disable-next-line
-  }, [id])
-
-  cmarked.setOptions({
-    highlight(code: string, lang: any) {
-      return Prism.highlight(code, Prism.languages[lang], lang)
-    }
-  })
-  const Post:React.FC = ()
-:
-        <div className="post">
-          <div className="title">{title}</div>
-          <div className="byline">
-            <div className="author">carrotWu</div>
-            <div className="time">{time}</div>
-          </div>
-          <div className="markdown-body" dangerouslySetInnerHTML={{__html:html}}/>
-        </div>
-      }
-
-    </div>
-  )
-}
-export default Post
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
+const a = 123
