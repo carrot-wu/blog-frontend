b1429bb7e53475d365a703fa5f116f56dbb1678a
wuhongrui <wuhongrui@ruqimobility.com>
2020-08-12 11:25:32 +0800

    chore: 修改开发代理地址

--- src/setuppRoxy.js
+++ src/setuppRoxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware')
 const isLocal = process.env.APIENV === 'local'
 module.exports = function(app) {
   app.use(proxy('/api/', {
-    target: isLocal? 'http://localhost:8082' : 'https://api.carrotwu.com',
+    target: isLocal? 'http://localhost:4000' : 'https://api.carrotwu.com',
     secure: false,
     changeOrigin: true,
     pathRewrite: { '/api': '' }
